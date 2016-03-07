14d14261033fbbe1b457cb98b6972b7138d5ee7c
chrisellis2005 <chrisellis2005@gmail.com>
2016-03-07 21:57:19 +0000

    added bootstrap styling

--- app/directives/reset-button.js
+++ app/directives/reset-button.js
@@ -2,7 +2,7 @@ angular.module("game-of-life")
     .directive("resetButton", function(){
         return {
             restrict: "E",
-            template: "<button ng-click=\"vm.reset()\" >Reset</button>",
+            template: "<button class=\"btn btn-primary btn-sm\" ng-click=\"vm.reset()\" >Reset</button>",
             controllerAs: "vm",
             scope: {
                 reset: "="
